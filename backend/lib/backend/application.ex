a1ccca48ee426775d53659d7ea32a7c17616af4e
programjm <programjm@gmail.com>
2019-11-02 13:25:22 -0500

    Configure application.ex

--- backend/lib/backend/application.ex
+++ backend/lib/backend/application.ex
@@ -2,11 +2,13 @@ defmodule Backend.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
-
   use Application
+  require Logger
+
 
   def start(_type, _args) do
     children = [
+      {Plug.Cowboy, scheme: :http, plug: Backend.HelloWorldPlug, options: [port: 8080]}
       # Starts a worker by calling: Backend.Worker.start_link(arg)
       # {Backend.Worker, arg}
     ]
@@ -14,6 +16,8 @@ defmodule Backend.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Backend.Supervisor]
+
+    Logger.info("Starting application...")
     Supervisor.start_link(children, opts)
   end
 end
