ed25058ab6daecb5b41b54e4e3ceb76f89825a36
Jacob <cooljacob.hill@gmail.com>
2019-11-02 20:03:13 -0500

    Database connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,26 @@
 // src/app.ts
 import bodyParser = require("body-parser");
 import express = require("express");
+import graphqlHTTP = require("express-graphql");
 import mysql = require("mysql");
 
 // Create a new express application instance
 const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "single_transferable",
+  insecureAuth: true
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to database!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
