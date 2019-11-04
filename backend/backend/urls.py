038b101f5384aea2ab68ccc0b8c91e3fa00435e1
programjm <programjm@gmail.com>
2019-11-03 18:48:32 -0600

    Refactor 'stv' to 'backend'

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""stv URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', GraphQLView.as_view(graphiql=True)),
 ]
